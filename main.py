006c92754d7a368553b0a32bdff03f2f2197668e
Kadir Şahin <kadirsahin_1395@hotmail.com>
2024-02-11 12:38:42 +0300

    add road line coord selection utils

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+from json import load, dump
 
 def video_frame_generator(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -47,6 +48,49 @@ class VideoWriter:
             self.out.release()
 
 
+def select_line(frame,window_msg):
+    points = []
+    quit_pressed = False
+    def mouse_callback(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            # add clicked point coordinates to the points list as tuple
+            points.append((x, y))
+            cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
+            cv2.imshow(window_msg, frame)
+
+    # create window and connect to the callback function
+    cv2.namedWindow(window_msg)
+    cv2.setMouseCallback(window_msg, mouse_callback)
+
+    while True:
+        cv2.imshow(window_msg, frame)
+        key = cv2.waitKey(1) & 0xFF
+        # exit when 'q' is pressed or when enough point to create a line is selected
+        if key == ord("q") or len(points) == 2:
+            break
+    if len(points) != 2: # quitted by 'q' press
+        cv2.destroyAllWindows()
+        quit_pressed = True
+        return None, quit_pressed
+    print("Selected line end coordinates:", points)
+
+    frame = plot_lines(frame, [points,])
+    return points, quit_pressed
+
+def select_lines(img,window_msg):
+    lines = []
+    quit_pressed = False
+
+    while(not quit_pressed):
+        line, quit_pressed = select_line(img,window_msg)
+        if line is not None:
+            lines.append(line)
+    return lines
+
+def plot_lines(img,line_coord_list):
+    for coord in line_coord_list:
+        cv2.line(img, *coord,  color=(255, 0, 0), thickness=2)
+    return img
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Car Direction Counting via Tracking"
@@ -65,6 +109,12 @@ def parse_arguments() -> argparse.Namespace:
         default="./data/video1_out.MOV",
         type=str,
     )
+    parser.add_argument(
+        "--select_road_lines_by_gui", default=False, help="Select road lines by selecting points on frame, if false, points are read from config.json", type=bool
+    )
+    parser.add_argument(
+        "--update_lines_in_config", default=False, help="Update the road line coords in config.json with new ones selected via mouse", type=bool
+    )
 
     return parser.parse_args()
 
@@ -72,9 +122,28 @@ if __name__ == "__main__":
     args = parse_arguments()
     
     frame_generator = video_frame_generator(args.source_video_path)
+
+    frame = next(frame_generator)
+    
+
+
+    if args.select_road_lines_by_gui:
+        road_vertical_line_coords = select_lines(frame,"Select road vertical reference lines from leftmost to rightmost. Press 'q' to quit.")
+        if args.update_lines_in_config:
+            with open("config.json","r+") as fp:
+                config = load(fp,)
+                config["road_vertical_line_coords"] = road_vertical_line_coords
+                fp.seek(0)
+                config = dump(config, fp)
+                fp.truncate()
+    else:
+        with open("config.json","r") as fp:
+            config = load(fp)
+            road_vertical_line_coords = config["road_vertical_line_coords"]
     
     with VideoWriter(args.target_video_path, fps = 30) as video_writer:
         for frame in frame_generator:
+            frame = plot_lines(frame, road_vertical_line_coords)
             cv2.imshow("Car Direction Counting", frame)
             video_writer.write_frame(frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
